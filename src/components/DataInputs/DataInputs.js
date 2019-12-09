5aeb13a89895c9a831755b3f58de5d8efa29e68c
Mike Lynch <mjlynch2@gmail.com>
2019-12-09 08:01:09 -0600

    improve styling

--- src/components/DataInputs/DataInputs.js
+++ src/components/DataInputs/DataInputs.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button, Typography } from "@material-ui/core";
+
+const styles = {
+    container: {
+        maxWidth: '75vw',
+        textAlign: 'center',
+        margin: 'auto',
+    },
+    el: {
+        margin: '5px'
+    }
+}
 
 class DataInputs extends Component {
   state = {
@@ -22,39 +33,55 @@ class DataInputs extends Component {
 
   render() {
     return (
-      <>
+      <div style={styles.container}>
         <TextField
           type="number"
           variant="outlined"
           label="Home Cost"
-          onChange={(event) => this.handleChange(event, "homeCost")}
+          onChange={event => this.handleChange(event, "homeCost")}
           placeholder="280000"
+          style={styles.el}
         />
         <TextField
           type="number"
           variant="outlined"
           label="Down Payment"
-          onChange={(event) => this.handleChange(event, "downPercent")}
+          onChange={event => this.handleChange(event, "downPercent")}
           placeholder="EG 0.20"
+          style={styles.el}
         />
         <TextField
           type="number"
           variant="outlined"
           label="Monthly Savings"
-          onChange={(event) => this.handleChange(event, "monthlySavings")}
+          onChange={event => this.handleChange(event, "monthlySavings")}
           placeholder="EG 2000"
+          style={styles.el}
         />
         <TextField
           type="number"
           variant="outlined"
           label="Starting Amount"
-          onChange={(event) => this.handleChange(event, "startingAmount")}
+          onChange={event => this.handleChange(event, "startingAmount")}
           placeholder="EG 10000"
+          style={styles.el}
         />
-        <Button onClick={this.handleSubmit}>Calculate!</Button>
-      </>
+        <Button
+          variant="outlined"
+          style={styles.el}
+          onClick={this.handleSubmit}
+        >
+          Calculate!
+        </Button>
+        <br/>
+        {this.props.data.months == undefined ? '' : <Typography variant='h3'>{this.props.data.months} Months Til House!</Typography>}
+      </div>
     );
   }
 }
 
-export default connect()(DataInputs);
+const mapStateToProps = reduxState => ({
+    data: reduxState.data
+})
+
+export default connect(mapStateToProps)(DataInputs);
